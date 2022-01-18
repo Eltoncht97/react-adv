389bc5bb863dcbbc3a0e758c669bda261a3766fb
Elton chavez <elton.mct@gmail.com>
2022-01-18 02:35:21 -0300

    fin del patron

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useCallback, useContext } from "react"
 import { ProductContext } from "./ProductCard"
 
 import styles from '../styles/styles.module.css'
@@ -10,7 +10,16 @@ export interface Props {
 
 export const ProductButtons = ({ className, style }:Props) => {
 
-  const { counter, increaseBy } = useContext(ProductContext)
+  const { counter, increaseBy, maxCount } = useContext(ProductContext)
+
+  const isMaxReached = useCallback(
+    () => !!maxCount && counter === maxCount,
+    [counter, maxCount],
+  );
+  // TODO: isMaxReched = useCallback, dependencias [ count, maxCount] 
+  //true si el count == max count
+  //false si no lo es
+
   
   return (
     <div className={ `${ styles.buttonsContainer } ${ className }` } style={style}>
@@ -19,7 +28,7 @@ export const ProductButtons = ({ className, style }:Props) => {
 
       <div className={ styles.countLabel }>{ counter }</div>
 
-      <button className={ styles.buttonAdd } onClick={ () => increaseBy(+1) }>+</button>
+      <button className={ `${ styles.buttonAdd } ${ isMaxReached() && styles.disabled }` } onClick={ () => increaseBy(+1) }>+</button>
 
     </div>
   )
--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { useProduct } from '../hooks/useProduct'
 import styles from '../styles/styles.module.css'
 
 import { createContext } from 'react'
-import { ProductContextProps, Product, onChangeArgs } from '../interfaces/interfaces';
+import { ProductContextProps, Product, onChangeArgs, InitialValues, ProductCartHandlers } from '../interfaces/interfaces';
 
 export const ProductContext = createContext({} as ProductContextProps);
 
@@ -11,29 +11,41 @@ const { Provider } = ProductContext
 
 export interface Props {
   product: Product;
-  children?: React.ReactElement | React.ReactElement[];
+  // children?: React.ReactElement | React.ReactElement[];
+  children: ( args: ProductCartHandlers) => JSX.Element;
   className?: string;
   style?: React.CSSProperties;
   onChange?: (args: onChangeArgs ) => void;
   value?: number;
+  initialValues?: InitialValues
 }
 
-export const ProductCard = ({ children, product, className, style, onChange, value }: Props) => {
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
 
-  const { counter, increaseBy } = useProduct({ onChange, product, value });
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProduct({ onChange, product, value, initialValues });
   
   return (
     <Provider value={{
       counter,
       increaseBy,
-      product
+      product,
+      maxCount
     }}>
       <div 
         className={ `${ styles.productCard } ${ className }` } 
         style={style}
       >
 
-        { children }
+        { 
+          children({
+            count: counter,
+            increaseBy,
+            isMaxCountReached,
+            maxCount: initialValues?.maxCount,
+            product, 
+            reset,
+          }) 
+        }
 
       </div>
     </Provider>
